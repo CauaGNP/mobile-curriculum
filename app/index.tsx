d3e5e73f819494f2938a0aa906e6af6681bb3fa5
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-10-23 23:24:10 +0000

    init structure

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Image, Text, View } from "react-native";
+import image from "../assets/hello.png"
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View>
+      <View>
+        <Text>Bem vindo ao meu portifolio</Text>
+        <Image source={image} style={styles.helloImage}/>
+      </View>
+      <View>
+        <Text>Sobre mim</Text>
+        <Text>Olá!! Eu sou o Cauã Gabriel, tenho 18 anos. Atualmente estou no 4° período do curso Sistemas para Internet na UNICAP.Sou desenvolvedor front-end, back-end. Gosto de criar soluções que unam tecnologia, criatividade e eficiência, e estou sempre buscando aprender coisas novas e me desafiar em projetos diferentes.</Text>
+      </View>
+      <View>
+        <Text>Tecnologias</Text>
+        <View>
+          <Text>Front-End</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ app/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    helloImage: {
+        height: 250,
+        width: 250,
+    }
+})
